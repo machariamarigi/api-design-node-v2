4262b02603f50e421b38202351b8622a8a6f620a
Macharia Marigi <macharia.marigi@andela.com>
2018-05-30 12:31:30 +0300

    express setup solution

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import http from 'http'
 import { createServer } from 'http'
 
 import app from './server'
+
+const server = http.createServer(app);
 let currentApp = app
 
+
+server.listen(3000, () => {
+	console.log('API on http://localhost:3000/')
+})
 
 if (module.hot) {
 	module.hot.accept(['./server'], () => {
--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
-const app = null
+const app = express();
 
-export default app
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hello world',
+    ok: true
+  });
+});
+
+export default app;
