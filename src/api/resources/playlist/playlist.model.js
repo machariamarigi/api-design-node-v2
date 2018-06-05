ee710255915bab1c36caa2eb8753935d53f1c158
Macharia Marigi <macharia.marigi@andela.com>
2018-06-05 13:04:35 +0300

    models

--- src/api/resources/playlist/playlist.model.js
+++ src/api/resources/playlist/playlist.model.js
@@ -1,7 +1,19 @@
 import mongoose from 'mongoose'
 
 export const schema = {
-  
+  title: {
+    type: String,
+    required: [true, 'Playlist requires a title']
+  },
+  songs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'song'
+  }],
+  favorite: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
 }
 
 const playlistSchema = new mongoose.Schema(schema)
--- src/api/resources/song/song.model.js
+++ src/api/resources/song/song.model.js
@@ -1,7 +1,23 @@
 import mongoose from 'mongoose'
 
 export const schema = {
-  
+  title: {
+    type: String,
+    required: [true, 'Song requires a title']
+  },
+  url: {
+    type: String,
+    unique: true,
+    required: [true, 'Url required']
+  },
+  album: String,
+  artist: String,
+  rating: {
+    type: Number
+  },
+  favorite: {
+    type: Boolean
+  },
 }
 
 const songSchema = new mongoose.Schema(schema)
